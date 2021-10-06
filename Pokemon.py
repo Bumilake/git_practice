99505d20f5a6d50962b2114728ac8500512cbeab
Bumilake <65740547+Bumilake@users.noreply.github.com>
2021-10-06 15:51:04 +0100

    New python program

---
+++ Pokemon.py
@@ -0,0 +1,8 @@
+class Pokemon:
+    def __init__(self, name, level, type, health):
+        self.name = name
+        self.health = health
+        self.level = level
+        maximum_level = 100
+        maximum_health = 5
+        
