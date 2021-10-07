31329fd055cd87a528bae6d5daf8b53400c41896
Bumilake <65740547+Bumilake@users.noreply.github.com>
2021-10-07 14:00:28 +0100

    New pygame

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import os
+import time
+import random
+
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Space Shooter Tutorial")
+
+#Load images
+
